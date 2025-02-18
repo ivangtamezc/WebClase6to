0a3efd26d43fe7e93fafac31c0cb1331af41ba32
Ivan Tamez <a00835985@tec.mx>
2025-02-18 16:19:19 -0600

    tarea de paginas seguras e inseguras

--- tarea login/clase 14 feb/script.js
+++ tarea login/clase 14 feb/script.js
@@ -5,7 +5,10 @@ function login() {
 
     
     if (user === "admin" && pass === "1234") {
-        window.location.href = "dashboard.html"; 
+        localStorage.setItem("auth", "true");
+         
+        alert("Inicio de sesión exitoso");
+        window.location.href = "dashboard.html";
     } 
     else if(user === "admin" && pass === "xd"){
         window.location.href = "dashboard.html"
@@ -14,3 +17,24 @@ function login() {
         errorMessage.style.display = "block";
     }
 }
+
+function checkAuth() {
+    if (localStorage.getItem("auth") !== "true") {
+        window.location.href = "login.html";
+        alert("No tienes acceso a esta página. Redirigiendo...");
+        
+    }
+    else{
+        window.location.href = "login.html";
+    }
+}
+
+
+
+
+function logout() {
+    localStorage.removeItem("auth"); // Borra la sesión
+    alert("Sesión cerrada");
+    window.location.href = "index.html";
+}
+
