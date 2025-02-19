2c71fb4ccfcab4bab6b355e015b6f05e96c2dd7f
Ivan Tamez <a00835985@tec.mx>
2025-02-19 09:56:50 -0600

    Agregue los comentarios faltantes

--- Tareas-De-Web/script.js
+++ Tareas-De-Web/script.js
@@ -1,40 +1,40 @@
 function login() {
     var user = document.getElementById("username").value;
     var pass = document.getElementById("password").value;
-    var errorMessage = document.getElementById("error-message");
+    var errorMessage = document.getElementById("error-message"); //Guardamos variables
 
     
-    if (user === "admin" && pass === "1234") {
+    if (user === "admin" && pass === "1234") { //Un usuario con acceso
         localStorage.setItem("auth", "true");
          
-        alert("Inicio de sesión exitoso");
+        alert("Inicio de sesión exitoso"); //Avisa que todo salio bien
         window.location.href = "dashboard.html";
     } 
-    else if(user === "admin" && pass === "xd"){
-        window.location.href = "dashboard.html"
+    else if(user === "admin1" && pass === "xd"){
+        window.location.href = "dashboard.html" //Otro usuario que podria ingresar
+        alert("Inicio de sesión exitoso");
+        window.location.href = "dashboard.html";
     }
     else {
-        errorMessage.style.display = "block";
+        errorMessage.style.display = "block"; //Muestra el mensaje de error
     }
 }
 
 function checkAuth() {
-    if (localStorage.getItem("auth") !== "true") {
-        window.location.href = "login.html";
-        alert("No tienes acceso a esta página. Redirigiendo...");
+    if (localStorage.getItem("auth") !== "true") { //Revisa si ya iniiaste seccion
+        window.location.href = "login.html"; //Si no iniciaste sesion te manda a la pagina de login
+        alert("No tienes acceso a esta página. Redirigiendo..."); //Te avisa que no has iniciado sesion y que no tienes acceso
         
     }
-    else{
-        window.location.href = "login.html";
-    }
+    
 }
 
 
 
 
-function logout() {
+function logout() { // Funcion para cerrar sesion
     localStorage.removeItem("auth"); // Borra la sesión
-    alert("Sesión cerrada");
-    window.location.href = "index.html";
+    alert("Sesión cerrada"); //Mensaje de que se cerro sesion
+    window.location.href = "index.html"; //Te regresa a la pagina de inicio
 }
 
